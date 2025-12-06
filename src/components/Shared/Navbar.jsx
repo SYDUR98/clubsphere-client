2013a7dd409c756903179a8ba0907be165e8a21b
sydur <eng.sydur@gmail.com>
2025-12-06 12:24:02 +0600

    added navbar

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  const manuLink = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `px-4 py-2 rounded-md font-medium transition-colors duration-300 ${
+              isActive ? "text-primary" : "text-base-content hover:text-primary"
+            }`
+          }
+        >
+          HOME
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {manuLink}
+          </ul>
         </div>
-    );
+        <div className="navbar-start">
+          <a
+            href="/"
+            className="btn btn-ghost normal-case flex flex-col items-start"
+          >
+            <span className="text-xl font-bold text-primary">ClubSphere</span>
+            <span className="text-sm text-base-content opacity-70">
+              Membership & Event Management for Local Clubs
+            </span>
+          </a>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{manuLink}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../components/Shared/Navbar';
 
 const MainLayout = () => {
     return (
         <div>
-            <h2 className='text-primary text-3xl bg-base-100'>main layout</h2>
-            <h2 className='text-secondary text-3xl bg-base-200'>main layout</h2>
-            <h2 className='text-base-content text-3xl bg-base-300'>main layout</h2>
-            <h2 className='text-primary-content text-3xl bg-base-300'>main layout</h2>
-            <h2 className='text-secondary-content text-3xl bg-base-300'>main layout</h2>
-            <h2 className='text-neutral text-3xl bg-base-300'>main layout</h2>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
